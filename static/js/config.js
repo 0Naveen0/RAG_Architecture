e1aa9328226fc0950a70c6cf68b1d12b91da5a3a
Naveen Kamal <naveenkamal.2011@gmail.com>
2026-04-22 10:03:17 +0000

    Base url in api.js changed to render

--- static/js/config.js
+++ static/js/config.js
@@ -1,4 +1,3 @@
-// export const API_BASE_URL = "http://127.0.0.1:5000"
-// export const API_BASE_URL = "http://127.0.0.1:56390"
-//export const API_BASE_URL = "https://rare-waves-wave.loca.lt"
-export const API_BASE_URL = "https://skinny-boaster-periscope.ngrok-free.dev" // NGROK URL
+
+//export const API_BASE_URL = "https://skinny-boaster-periscope.ngrok-free.dev" // NGROK URL
+export const API_BASE_URL = "https://rag-architecture-v2.onrender.com" // Render URL
