caf9e17cd688cce87b52bd00bec46e2e59843231
Naveen Kamal <naveenkamal.2011@gmail.com>
2026-04-23 15:21:42 +0000

    gunicorn worker class changed

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,11 +1,11 @@
 import os
 
 workers = 1
-threads = 2
-worker_class = "gthread"
+threads = 1
+worker_class = "sync"
 timeout = 120 #for groq/gemini api calls
 max_requests = 50  # to prevent memory leak
 max_request_jitter = 10
 # bind = "0.0.0.0:8000"
 bind = f"0.0.0.0:{os.environ.get('PORT', 10000)}"
-preload_app = False #load modal once before fork
+preload_app = True #load modal once before fork
